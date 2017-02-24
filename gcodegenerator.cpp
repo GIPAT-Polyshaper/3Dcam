397572ab0f580153e43d2f563733a1762f037568
flavioprattico <flavioprattico@gmail.com>
2017-02-24 12:47:56 +0100

    Load and read stl file. Added a.stl to the main folder. Be careful: change the absolute path to the stl file in gcodegenerator.cpp

--- gcodegenerator.cpp
+++ gcodegenerator.cpp
@@ -1,5 +1,7 @@
 #include "gcodegenerator.h"
 #include <iostream>
+#include <QFile>
+#include <QDataStream>
 
 GCodeGenerator::GCodeGenerator()
 {
@@ -8,5 +10,38 @@ GCodeGenerator::GCodeGenerator()
 
 void GCodeGenerator::readAndGenerate()
 {
-    std::cout << "Eccomi" << std::endl;
+    //std::cout << "a soreta" << std::endl;
+
+    // cambiare il path assoluto per chi fa prove
+    QFile file("/Users/flavioprattico/Desktop/a.stl");
+    file.open(QIODevice::ReadOnly);
+
+
+    if( !file.exists() )
+      {
+        std::cout << "Il file non c'è" << std::endl;
+      }
+
+    // Skip the rest of the header material
+    file.read(75);
+
+    QDataStream data(&file);
+    data.setByteOrder(QDataStream::LittleEndian);
+    data.setFloatingPointPrecision(QDataStream::SinglePrecision);
+
+    // Load the triangle count from the .stl file
+    uint32_t tri_count;
+    data >> tri_count;
+
+    std::cout << tri_count << std::endl;
+
+    // Extract vertices into an array of xyz, unsigned pairs
+    //QVector<Vec3i> verts(tri_count*3);
+
+
+
+
+
+
+
 }
