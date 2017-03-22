170d10bda8079780f9a851b9c68e44ce13033a82
flavioprattico <flavioprattico@gmail.com>
2017-03-22 09:31:44 +0100

    Upper profile detection algorithm implementation. It works, more or less...

--- 3dcamlib/gcodegenerator.cpp
+++ 3dcamlib/gcodegenerator.cpp
@@ -27,19 +27,19 @@
 #include <QDataStream>
 #include <QVector>
 #include <typeinfo>
-//#include <algorithm> per la funzione sort
+#include <algorithm>
+#include <vector>
 
 struct Vec3
 {
-    float x, y, z;
+    float y, z, ind;
 };
 
 struct Vec3i {
     Vec3 first;
-    uint second;
 
     bool operator<(const Vec3i& other) const {
-        return first.x < other.first.x;
+        return first.y < other.first.y;
     }
 
     //bool operator==(const Vec3i& other) const {
@@ -58,102 +58,77 @@ void GCodeGenerator::readAndGenerate()
 {
     //std::cout << "a soreta" << std::endl;
 
-    // cambiare il path assoluto per chi fa prove
-    QFile file("/Users/flavioprattico/Desktop/a.stl");
-    file.open(QIODevice::ReadOnly);
+    float indi [11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    float yi [11]   = {10, 5, 3, 1, 2, 4, 6, 8, 9, 7, 10};
+    float zi [11]   = {1, 1, 2, 2, 4, 6, 7, 5, 3, 2, 2};
 
 
-    if( !file.exists() )
-      {
-        std::cout << "Il file non c'è" << std::endl;
-      }
+    QVector<Vec3i> verts(11);
 
-    // Skip the header material
-    file.read(80);
-
-    QDataStream data(&file);
-    data.setByteOrder(QDataStream::LittleEndian);
-    data.setFloatingPointPrecision(QDataStream::SinglePrecision);
-
-    // Load the triangle count from the .stl file
-    uint32_t tri_count;
-    data >> tri_count;
-
-    //std::cout << typeid(data).name() << std::endl;
-
-    // Extract vertices into an array of xyz, unsigned pairs
-    QVector<Vec3i> verts(tri_count*3);
-
-    // Dummy array, because readRawData is faster than skipRawData
-    char buffer[sizeof(float)*3];
-
-    // Store vertices in the array, processing one triangle at a time.
-    for (auto v=verts.begin(); v != verts.end(); v += 3)
-    {
-        // Skip face's normal vector
-        data.readRawData(buffer, 3*sizeof(float));
-
-        // Load vertex data from .stl file into vertices
-        data >> v[0].first.x >> v[0].first.y >> v[0].first.z;
-        data >> v[1].first.x >> v[1].first.y >> v[1].first.z;
-        data >> v[2].first.x >> v[2].first.y >> v[2].first.z;
-
-        // Skip face attribute
-        data.readRawData(buffer, sizeof(uint16_t));
-    }
-
-    // Save indicies as the second element in the array
-    // (so that we can reconstruct triangle order after sorting)
-    for (size_t i=0; i < tri_count*3; ++i)
-    {
-        verts[i].second = i;
-    }
 
 
     // da qui in poi procedo per la mia strada (finalmente)
 
-    std::sort(verts.begin(), verts.end());
 
-    // odio le strutture quindi mi costruisco i tre vettori xx, yy, zz e ind
-
-    float xx[verts.length()];
-    float yy[verts.length()];
-    float zz[verts.length()];
-    float ind[verts.length()];
 
     for (int i=0; i < verts.length(); i++)
     {
         //std::cout << verts[i].first.x << " - " << verts[i].first.y << std::endl;
 
-        xx[i] = verts[i].first.x;
-        yy[i] = verts[i].first.y;
-        zz[i] = verts[i].first.z;
-        ind[i] = i;
+        verts[i].first.y = yi[i];
+        verts[i].first.z = zi[i];
+        verts[i].first.ind = indi[i];
+        //std::cout << verts[i].first.y << " - " << verts[i].first.ind << std::endl;
     }
 
+    std::sort(verts.begin(), verts.end());
 
-// discretization as a function of the tool dimension
+/*
+//show ordered struct
 
-    float toolD = 0.2;
-
-    int xxd[verts.length()];
-
-//    std::cout << *std::min_element(xx,xx+verts.length()) << '\n';
-//    std::cout << *std::max_element(xx,xx+verts.length()) << '\n';
-
-    float x_min = *std::max_element(xx,xx+verts.length());
-    float x_max = *std::max_element(xx,xx+verts.length());
-
-    /*
     for (int i=0; i < verts.length(); i++)
     {
-        //std::cout << verts[i].first.x << std::endl;
 
-
-
+       std::cout << verts[i].first.y << " - " << verts[i].first.ind << std::endl;
     }
+
+
 */
 
+
+
+    // let hypothesize clockwise increment in the yz profile
+
+    std::vector<float> surfy;
+    std::vector<float> surfz;
+    std::vector<float> surfind;
+
+
+    float tmp = verts[0].first.ind;
+
+    for (int i=1; i < verts.length(); i++)
+    {
+        if (tmp+1==verts[i].first.ind || verts[i].first.ind==1){
+
+            if (verts[i-1].first.y <= verts[i].first.y){
+
+                surfy.push_back(verts[i].first.y);
+                surfz.push_back(verts[i].first.z);
+                surfind.push_back(verts[i].first.ind);
+                tmp = verts[i].first.ind;
+            }
+
+
+        }
+        //std::cout << surfy[i] << " - " << surfind[i] << std::endl;
+    }
+
+
+    for (int i=0; i < 7; i++)
+    {
+
+        std::cout << surfy[i] << " - " << surfind[i] << std::endl;
+    }
 
 
 
