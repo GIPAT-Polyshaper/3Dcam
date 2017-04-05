832c1e2bdfad1423b6b91d68facdd3ddfdd45aa5
flavioprattico <flavioprattico@gmail.com>
2017-04-05 11:57:58 +0200

    Fixed test problem. The algorith works for every test until "tool path triangle starting from any point"

--- 3dcamlib/undersquare.cpp
+++ 3dcamlib/undersquare.cpp
@@ -10,16 +10,18 @@ Undersquare::Undersquare(std::vector<double> y, std::vector<double> z)
     if (!y.empty())
     {
         int start = 0;
-        double min = y[0];
+        double min = y[start];
 
         // find the minimun value in y
         for(int j=1; j<y.size();j++)
             {
 
-            if(y[j] < min)
+
+            if(y[j] < min){
 
                 min = y[j];
                 start = j;
+                }
 
             }
 
@@ -28,7 +30,9 @@ Undersquare::Undersquare(std::vector<double> y, std::vector<double> z)
         ty[0] = y[start];
         tz[0] = z[start];
 
-        // start:y.size, 0:start
+
+        //std::cout << start << std::endl;
+        //std::cout << y[start] << " - " << z[start] << std::endl;
 
     for (auto i = 1; i < y.size(); ++i) {
         const auto j = (i + start) % y.size();
@@ -36,6 +40,7 @@ Undersquare::Undersquare(std::vector<double> y, std::vector<double> z)
 
         if (y[j] >= y[j-1])
         {
+            //std::cout << y[j] << " - " << z[j] << std::endl;
             ty.push_back(y[j]);
             tz.push_back(z[j]);
         }
