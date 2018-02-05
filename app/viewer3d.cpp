4643923ae1865bdfa71e0921f41dd9311d8c0f6b
Piero Collevecchio <pierokoll@gmail.com>
2018-02-05 13:05:13 +0100

    clean viewer3d code

--- app/viewer3d.cpp
+++ app/viewer3d.cpp
@@ -54,40 +54,4 @@ Viewer3D::Viewer3D()
 {
     setMirrorVertically(true);
 }
-/*
-   * here only for reference
-   */
-//class FbItemRenderer : public QQuickFramebufferObject::Renderer
-//{
-//    QOpenGLFramebufferObject *createFramebufferObject(const QSize &size)
-//    {
-//        QOpenGLFramebufferObjectFormat format;
-//        format.setAttachment(QOpenGLFramebufferObject::CombinedDepthStencil);
-//        // optionally enable multisampling by doing format.setSamples(4);
-//        return new QOpenGLFramebufferObject(size, format);
-//    }
-
-//    void render()
-//    {
-//        // Called with the FBO bound and the viewport set.
-//        ... // Issue OpenGL commands.
-//    }
-//    ...
-//}
-
-//class FbItem : public QQuickFramebufferObject
-//{
-//    QQuickFramebufferObject::Renderer *createRenderer() const
-//    {
-//        return new FbItemRenderer;
-//    }
-//    ...
-//}
-
-//int main(int argc, char **argv)
-//{
-//    ...
-//    qmlRegisterType("fbitem", 1, 0, "FbItem");
-//    ...
-//}
 
