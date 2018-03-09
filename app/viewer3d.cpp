105d9b1a4141b6a6b73f9f5ac3d6ee18cbe3bc03
Piero Collevecchio <pierokoll@gmail.com>
2018-03-09 10:31:54 +0100

    refactoring Viewer3D

--- app/viewer3d.cpp
+++ app/viewer3d.cpp
@@ -7,6 +7,8 @@
 class Model3DRenderer : public QQuickFramebufferObject::Renderer
 {
 public:
+    StlRenderer obj;
+
     Model3DRenderer()
     {
         obj.initialize();
@@ -22,18 +24,31 @@ public:
         bool camera_dirty = GCodeGenerator::get_instance().isCameraDirty();
         bool triangles_dirty = GCodeGenerator::get_instance().isTrianglesDirty();
         bool volume_dirty = GCodeGenerator::get_instance().isVolumeDirty();
+
+        //if we have a new model to render, or working volume has changed
         if (triangles_dirty || volume_dirty)
         {
             obj.setGeometry(GCodeGenerator::get_instance().getTriangles());
-            obj.setCamera(GCodeGenerator::get_instance().getAzimuth(), GCodeGenerator::get_instance().getDistance(), GCodeGenerator::get_instance().getElevation());
-            obj.setVolume(GCodeGenerator::get_instance().getVolumeX(), GCodeGenerator::get_instance().getVolumeY(), GCodeGenerator::get_instance().getVolumeZ());
+
+            obj.setCamera(GCodeGenerator::get_instance().getAzimuth(),
+                          GCodeGenerator::get_instance().getDistance(),
+                          GCodeGenerator::get_instance().getElevation());
+
+            obj.setVolume(GCodeGenerator::get_instance().getVolumeX(),
+                          GCodeGenerator::get_instance().getVolumeY(),
+                          GCodeGenerator::get_instance().getVolumeZ());
+
             GCodeGenerator::get_instance().cleanVolume();
             GCodeGenerator::get_instance().cleanCamera();
             GCodeGenerator::get_instance().cleanTriangles();
         }
+        //if camera position has changed
         if (camera_dirty)
         {
-            obj.setCamera(GCodeGenerator::get_instance().getAzimuth(), GCodeGenerator::get_instance().getDistance(), GCodeGenerator::get_instance().getElevation());
+            obj.setCamera(GCodeGenerator::get_instance().getAzimuth(),
+                          GCodeGenerator::get_instance().getDistance(),
+                          GCodeGenerator::get_instance().getElevation());
+
             GCodeGenerator::get_instance().cleanCamera();
         }
     }
@@ -44,7 +59,6 @@ public:
         format.setSamples(4);
         return new QOpenGLFramebufferObject(size, format);
     }
-    StlRenderer obj;
 };
 
 QQuickFramebufferObject::Renderer *Viewer3D::createRenderer() const
@@ -56,4 +70,3 @@ Viewer3D::Viewer3D()
 {
     setMirrorVertically(true);
 }
-
